d73fbdbf7b088a3ff2b6d808e1b4ee9f1cb0b6cd
tainna <64937004+tainna@users.noreply.github.com>
2025-08-18 21:56:14 +0200

    Add files via upload

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taalmeid <taalmeid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 10:43:55 by taalmeid          #+#    #+#             */
+/*   Updated: 2025/07/23 13:56:06 by taalmeid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		--letter;
+	}
+}
+
+	// int main(void)
+	// {
+	// ft_print_reverse_alphabet();
+	// return (0);
+	// }
